758dc010a0769e7288aa3512f0f36ec464175d97
Rik Roots <rik.roots@gmail.com>
2022-07-19 21:03:55 +0100

    bump to v8.9.2

--- src/components/FooterLinks.svelte
+++ src/components/FooterLinks.svelte
@@ -22,7 +22,7 @@
     }
 </style>
 
-<p>Scrawl-canvas version 8.9.1 - released 13 Jul 2022. Please raise issues in the Discord channel, or Github.</p>
+<p>Scrawl-canvas version 8.9.2 - released 13 Jul 2022. Please raise issues in the Discord channel, or Github.</p>
 <nav>
     <div>
         <a href="/">Tour</a>
@@ -35,7 +35,7 @@
         <a rel="external" href="https://discord.gg/DcwfkZDuBV">Discord channel</a>
         <a rel="external" href="https://github.com/KaliedaRik/Scrawl-canvas">GitHub Repo</a>
         <a rel="external" href="https://github.com/KaliedaRik/Scrawl-canvas/issues">Issues & Bugs</a>
-        <a download href="/downloads/scrawl-canvas_8-9-1.zip">Download v8.9.1</a>
+        <a download href="/downloads/scrawl-canvas_8-9-2.zip">Download v8.9.2</a>
     </div>
     <div>
         <a href="/privacy-and-cookies">Privacy + Cookies</a>
